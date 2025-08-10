91713a11ced022a1d6a985e39abb742feb7aeda7
Ankit <ankit.sangode@gmail.com>
2025-08-11 03:30:42 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sentiment_analysis.logger.logger_setup import logger
 from sentiment_analysis.exception.customException import CustomException
 from sentiment_analysis.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from sentiment_analysis.pipeline.stage_02_data_validation import DataValidationPipeline
 import sys
 
 STAGE_NAME = 'Data Ingestion'
@@ -9,6 +10,18 @@ if __name__ == "__main__":
     try:
         logger.info(f">>>>>>>>>>>{STAGE_NAME}<<<<<<<<<< Started")
         obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>>>>>>>>>>{STAGE_NAME} completed <<<<<<<<<<<<<")
+    except CustomException as e:
+        raise CustomException(e,sys)
+    
+
+STAGE_NAME = 'Data Ingestion'
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>>>>>>>{STAGE_NAME}<<<<<<<<<< Started")
+        obj = DataValidationPipeline()
         obj.main()
         logger.info(f">>>>>>>>>>>>>{STAGE_NAME} completed <<<<<<<<<<<<<")
     except CustomException as e:
